fa42df2e4312ddc3b09033671764c1e4de718946
user <user@fvfxd0z4j9k2.localdomain>
2019-01-09 17:10:26 -0500

    Added steps block

--- node_js-docker.groovy
+++ node_js-docker.groovy
@@ -2,7 +2,9 @@ pipeline {
     agent any
     stages {
         stage('Checkout'){
-            git url: 'https://github.com/wardviaene/node-demo-app.git'
+            steps{
+                git url: 'https://github.com/wardviaene/node-demo-app.git'
+            }
         }
     }
 }
