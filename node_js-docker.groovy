4d8728e197479df2fd08292ab3a05d22dc5560d2
adityaTor <adityatrnla@outlook.com>
2019-01-10 15:58:44 -0500

    added dependencies stage

--- node_js-docker.groovy
+++ node_js-docker.groovy
@@ -6,5 +6,10 @@ pipeline {
                 git url: 'https://github.com/wardviaene/node-demo-app.git'
             }
         }
+        stage('Install Dependencies') {
+            steps {
+                sh 'npm install'
+            }
+        }
     }
 }
